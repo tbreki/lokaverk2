b50a7381ee9b788fd954e633b487aaf878053112
Þ Breki <93223461+tbreki@users.noreply.github.com>
2023-07-17 16:31:32 +0000

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import Navigation from '@/components/Navigation'
-import Image from 'next/image'
-
-
-
+import React from "react";
+import Navigation from "@/components/Navigation";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    
-    
     <main>
-     <div className="flex sm:justify-center space-x-4">
-     
-     </div>
-     
+      <div className="flex sm:justify-center space-x-4"></div>
 
-    
-    <h1 className='flex place-content-center text-3xl text-maingreen font-bold m-10 '>WELCOME TO LIL'BITS</h1>
-    
-    <div className=' bg-maingreen height-50% mt-10'> 
-   
+      <h1 className="flex place-content-center text-3xl text-maingreen font-bold m-10 ">
+        WELCOME TO LIL'BITS
+      </h1>
 
-   <div className='flex place-content-center'>
-   <Image
-      src="/logo.png"
-      width={500}
-      height={500}
-      alt="logo"
-    />
-   </div>
-    </div>
-  
-  
-  
-  </main>
-  )
+      <div className=" bg-maingreen height-50% mt-10">
+        <div className="flex place-content-center">
+          <Image src="/img2.jpg" width={500} height={500} alt="logo" />
+        </div>
+      </div>
+
+      <div className="flex sm: justify-center mt-5 ">
+        {" "}
+        <label for="email">Enter your email:</label>
+        <input
+          type="email"
+          id="email"
+          pattern=".+@globex\.com"
+          size="30"
+          required
+        ></input>
+      </div>
+      <div className="flex place-content-center">
+        <button class="rounded-md bg-maingreen/20 hover:bg-maingreen">
+          Confirm
+        </button>
+      </div>
+    </main>
+  );
 }
--- pages/Recept.js
+++ pages/Recept.js
@@ -2,20 +2,39 @@ import React from "react";
 import DrinkDetails from "@/components/DrinkName";
 import MealDetails from "@/components/MealName";
 import DatelDetails from "@/components/DateDetails";
-import "../app/globals.css"
-
+import "../app/globals.css";
 
 export default function recept() {
   return (
     <main className="flex sm:justify-center mt-10">
-    <div className="block max-w-sm p-6 bg-white border border-gray-200 
-    rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" >
-        <div >Meal:<h2 className="text font-semibold"> <MealDetails/></h2></div>
-        <div >Drink:<h2 className="text font-semibold"> <DrinkDetails/></h2></div>
-        <div >Date:<h2 className="text font-semibold"> <DatelDetails/></h2></div>
-        <h2 className="text font-light">-----------------</h2>
-       <h2 className="text font-semibold">Price: 1.000.000 USD</h2>
+      <div
+        className="block max-w-sm p-6 bg-white border border-gray-200 
+    rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <div>
+          Meal:
+          <h2 className="text font-semibold">
+            {" "}
+            <MealDetails />
+          </h2>
         </div>
+        <div>
+          Drink:
+          <h2 className="text font-semibold">
+            {" "}
+            <DrinkDetails />
+          </h2>
+        </div>
+        <div>
+          Date:
+          <h2 className="text font-semibold">
+            {" "}
+            <DatelDetails />
+          </h2>
+        </div>
+        <h2 className="text font-light">-----------------</h2>
+        <h2 className="text font-semibold">Price:777 EUR</h2>
+      </div>
     </main>
   );
 }
