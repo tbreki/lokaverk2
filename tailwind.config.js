896a6966c75d9821a6b443cba456f281f64eae8e
Þ Breki <93223461+tbreki@users.noreply.github.com>
2023-07-17 16:32:15 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
-    colors:{
-      "maingreen": "#436053",
-      "mainred": "#e11d48",
-      "mainbase": "#fef3c7"
-
-    }
+    extend: {
+      colors: {
+        maingreen: "#436053",
+        mainred: "#e11d48",
+        mainbase: "#fef3c7",
+      },
+    },
   },
-  plugins: [],
-}
+};
